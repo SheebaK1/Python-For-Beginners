f13bc0b43f85cbbc17960afd0eb155e6bf86ad98
Sheeba <sheebakhan.mca@gmail.com>
2024-09-04 12:15:51 +0100

    Adding Hello-World.py,Comment.py,keywords.py files

---
+++ Keywords.py
@@ -0,0 +1,9 @@
+# Keywords are the reserved words in Python and it can not be used as an identifier in python programming language.
+
+import keyword
+
+print(keyword.kwlist)
+
+# Print the Lenght of the keyword available in python
+
+print(len(keyword.kwlist))
